e37cc068bbbf1e38000765f6309c20894104c1c9
OuerdiaSlm <ouerdia.slm@gmail.com>
2022-12-02 16:19:30 +0100

    Updates

--- mon-app/src/components/carrousel/carrousel.jsx
+++ mon-app/src/components/carrousel/carrousel.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react"
 import './carrousel.css';
+import logo from '../../assets/logo.png';
 import banner from '../../assets/banner.jpg';
+import cadre from '../../assets/cadre.jpg';
 import data from '../../datas/datas.json';
 import amechiche from '../../assets/logo.png';
 import { Carousel } from 'react-responsive-carousel';
 
   function Carrousel (){
+
+    let [bannerWeb,setBanner]=useState(banner)
 
     let slide=['../../assets/logo.png', "../../assets/plante.png", "../../assets/entree.png"]
 
@@ -26,13 +30,22 @@ import { Carousel } from 'react-responsive-carousel';
         console.log(imageSlide)
   }
 
+  window.onscroll = ()=>{
+    if (document.body.scrollTop > 580 || document.documentElement.scrollTop > 580) {
+      setBanner(cadre)
+    }
+    else{
+      setBanner(banner)
+    }
+  }
+
       return (
             <section className="bannerDiv">      
               <div className="test1">
-                  <img src={banner} className="banner" alt="La forêt de peupliers"/>
+                  <img src={bannerWeb} className="banner" alt="La forêt de peupliers"/>
               </div>
               <div className="test2">
-                  <p className="bannerText">BIENVENU CHEZ IMAGINE !</p>
+                  <p className="bannerText">BIENVENU CHEZ IMAGINE</p>
               </div> 
             </section>
       );
--- mon-app/src/pages/home.jsx
+++ mon-app/src/pages/home.jsx
@@ -3,6 +3,8 @@ import Carrousel from "../components/carrousel/carrousel";
 import OurFood from "../components/ourFood/ourFood";
 
 function Home () {
+
+
   return(
     <>
       <Carrousel/>
